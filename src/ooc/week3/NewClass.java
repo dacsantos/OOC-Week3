77c75585cc0867ed1b8fee3f9338bc2c8f62a2fc
dacsantos <72028806+dacsantos@users.noreply.github.com>
2021-10-04 12:45:43 +0100

    V2
    This is a test. Added a new class.

---
+++ src/ooc/week3/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ooc.week3;
+
+/**
+ *
+ * @author dacsa
+ */
+public class NewClass {
+    
+    public NewClass(){
+        System.out.println("This is a new constructor");
+    }
+    
+}
