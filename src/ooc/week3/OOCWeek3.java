285d54079d0af2d2473dfc22b1f8dddb659dcecf
dacsantos <72028806+dacsantos@users.noreply.github.com>
2021-10-04 13:29:54 +0100

    Testing conflict
    testing

--- src/ooc/week3/OOCWeek3.java
+++ src/ooc/week3/OOCWeek3.java
@@ -8,8 +8,8 @@ public class OOCWeek3 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("This is my first repository");
-        System.out.println("This is my first repository");
+        System.out.println("This is a test");
+   
       
         System.out.println("This is my first repository");
         System.out.println("This is my first repository");
